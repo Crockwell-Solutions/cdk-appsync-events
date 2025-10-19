c53393470aeb29e10c6753ae8144dec0e3fdeb61
Ian <ian@crockwellsolutions.com>
2025-10-19 22:46:07 +0100

    Added route assessment to enrichment Lambda function

--- src/event-processing/hazard-enrichment.ts
+++ src/event-processing/hazard-enrichment.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /*
  * Hazard Enrichment Lambda
  *
@@ -8,18 +9,32 @@
  */
 
 import { unmarshall } from '@aws-sdk/util-dynamodb';
-import { logger, Point, getRouteDistance, createRouteRecord } from '../shared';
+import { logger, Point, isPointNearRoute } from '../shared';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb';
+
+const AIRSPACE_ALERTER_TABLE = process.env.AIRSPACE_ALERTER_TABLE;
 
 const client = new DynamoDBClient({});
 const ddb = DynamoDBDocumentClient.from(client);
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const handler = async (event: any): Promise<any> => {
   logger.info('Processing new hazard event', { event });
 
   const returnedEvents = [];
+
+  // Fetch all the routes from DynamoDB
+  const queryInput: any = {
+    TableName: AIRSPACE_ALERTER_TABLE,
+    Limit: 50,
+    KeyConditionExpression: 'PK = :pk',
+    ExpressionAttributeValues: {
+      ':pk': 'ROUTE',
+    },
+  };
+  const queryParams = new QueryCommand(queryInput);
+  const routes: any = await ddb.send(queryParams);
+  logger.info('Fetched routes from DynamoDB for enrichment', { routeCount: routes.Items?.length });
 
   for (const record of event) {
     // Unmarshall the DynamoDB record
@@ -36,6 +51,24 @@ export const handler = async (event: any): Promise<any> => {
         },
       }),
     );
+
+    // Check if the hazard affects any routes and add this to any responses as an alert
+    const hazardPoint: Point = { lat: newRecord.lat, lon: newRecord.lon };
+    for (const route of routes.Items || []) {
+      if (isPointNearRoute(route.routePoints, hazardPoint)) {
+        returnedEvents.push(
+          JSON.stringify({
+            type: 'Alert',
+            data: {
+              routeId: route.SK.split('#')[1],
+              type: newRecord.type,
+              lat: newRecord.lat,
+              lon: newRecord.lon,
+            },
+          }),
+        );
+      }
+    }
   }
 
   return {
